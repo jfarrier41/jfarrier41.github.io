eaaa57ad6efff560e519bee6192e4701ca6fea28
jfarrier41 <jfarrier@carroll.edu>
2024-04-25 10:04:50 -0600

    Added js script and formatted

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// YYour  code, essenntially helps the page load better
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navMenu = document.querySelector('.nav-menu');
+
+  menuToggle.addEventListener('click', function() {
+      navMenu.classList.toggle('active');
+  });
+});
